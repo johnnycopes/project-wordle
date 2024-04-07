f0865b28098c1b93aadf0e435048feb022e40510
John Coppola <jdcoppola@gmail.com>
2024-04-07 13:48:12 -0400

    FEATURE: Guess letter logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,7 +3,9 @@ import React from "react";
 function Guess({ id, guess }) {
   return (
     <p key={id} className="guess">
-      {guess}
+      {guess.split("").map((letter) => (
+        <span className="cell">{letter}</span>
+      ))}
     </p>
   );
 }
