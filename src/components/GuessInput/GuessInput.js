ccf034df0c5eedee9d2cdd19cb0cc616ed47280b
John Coppola <jdcoppola@gmail.com>
2024-04-07 12:18:10 -0400

    FEATURE: implements GuessInput logic

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
 
-function GuessInput() {
-  return <div></div>;
+function Guess({ submit }) {
+  const [guess, setGuess] = React.useState("");
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        submit(guess);
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        pattern="[A-Z]{5}"
+        value={guess}
+        title="5 letter word"
+        required
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
 }
 
-export default GuessInput;
+export default Guess;
