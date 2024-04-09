378c5a19d908bf9800241262b57f9e4912b6f575
John Coppola <jdcoppola@gmail.com>
2024-04-08 22:23:51 -0400

    FEATURE: fleshes out Banner contents

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-function Banner() {
-  return <div></div>;
+function Banner({ outcome }) {
+  const win = (
+    <div class="happy banner">
+      <p>
+        <strong>Congratulations!</strong> Got it in
+        <strong>3 guesses</strong>.
+      </p>
+    </div>
+  );
+  const lose = (
+    <div class="sad banner">
+      <p>
+        Sorry, the correct answer is <strong>LEARN</strong>.
+      </p>
+    </div>
+  );
+
+  return outcome === "win" ? win : lose;
 }
 
 export default Banner;
