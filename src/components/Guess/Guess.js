30bff1c75f69598881b0a09eb903247af52c9573
John Coppola <jdcoppola@gmail.com>
2024-04-08 20:02:22 -0400

    FEATURE: displays status for each guess letter

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,18 @@
 import React from "react";
 
+import { checkGuess } from "../../game-helpers";
 import { range } from "../../utils";
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+  const diff = checkGuess(guess, answer);
+
   return (
     <p className="guess">
       {range(5).map((position) => (
-        <span key={position} className="cell">
+        <span
+          key={position}
+          className={`cell ${diff ? diff[position].status : ""}`}
+        >
           {guess ? guess[position] : undefined}
         </span>
       ))}
--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Guess from "../Guess/Guess";
 
-function Guesses({ guesses }) {
+function Guesses({ guesses, answer }) {
   return (
     <div className="guess-results">
       {guesses.map((guess, index) => (
-        <Guess key={index} guess={guess} />
+        <Guess key={index} guess={guess} answer={answer} />
       ))}
     </div>
   );
